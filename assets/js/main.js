ef9f4f5ef15d8bea3c58293ad01d0f71fe03cb49
Isabella Sanchis <isasanchis3@gmail.com>
2022-09-29 09:07:46 -0300

    deploy

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//const e let nao tem hoisting
+const nome = "Fiap";
+console.log(nome);
+
+escreve("exemplo");
+//função nominal - udf
+function escreve(titulo) {
+    alert(titulo)
+}
+
+//função anônima
+
+//arrow function
+
+//
